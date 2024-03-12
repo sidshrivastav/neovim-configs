6603f49ba3d81d85592d007ee0b0e7004b99c412
Siddhant Shrivastav <sidshrivastav1002@gmail.com>
2024-03-12 21:45:23 +0530

    feat: added lazygit inside the nvim

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,17 @@
+return {
+  "kdheepak/lazygit.nvim",
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  -- optional for floating window border decoration
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    -- vim.keymap.set("n", "<C-z>", ":LazyGit<CR>", {})
+  end,
+}
