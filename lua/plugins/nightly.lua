61562e99ab7dd877ed90693412742c66e9ceba2e
Siddhant Shrivastav <sidshrivastav1002@gmail.com>
2024-04-04 12:10:35 +0530

    chore: removed neo tree

---
+++ lua/plugins/nightly.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"Alexis12119/nightly.nvim",
+		config = function()
+			require("nightly").setup({
+				transparent = false,
+				styles = {
+					comments = { italic = true },
+					functions = { italic = false },
+					variables = { italic = false },
+					keywords = { italic = false },
+				},
+				highlights = {},
+			})
+			-- vim.cmd.colorscheme("nightly")
+		end,
+	},
+}
